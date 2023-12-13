4f3b9e4fc589c6fc61a94d4904ff541eb9e4f4e7
Alexander Lazarov <alexander.lazaroff@gmail.com>
2023-12-13 08:13:01 +0200

    Remove useless split() and join()

--- 2023/13/solution.rb
+++ 2023/13/solution.rb
@@ -4,10 +4,10 @@ INPUT = 'input.txt'
 patterns = File.read(INPUT).split("\n\n").map { _1.split("\n").map { |row| row.split('') } }
 
 def diff(r, x)
-  a, b = r[0...x].reverse.join(''), r[x..].join('')
+  a, b = r[0...x].reverse, r[x..]
   m = [a.size, b.size].min
 
-  a[0...m].chars.zip(b[0...m].chars).count { _1 != _2 }
+  a[0...m].zip(b[0...m]).count { _1 != _2 }
 end
 
 def col(p, d) = (1...(p.first.size)).to_a.find { |i| p.sum { diff(_1, i) } == d } || 0
