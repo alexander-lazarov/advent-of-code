9a304f96b4826af143230212b174b03e7adf37d7
Alexander Lazarov <alexander.lazaroff@gmail.com>
2023-12-13 07:46:03 +0200

    Add solution to 2023/13

---
+++ 2023/13/solution.rb
@@ -0,0 +1,17 @@
+# INPUT = 'input-sample.txt'
+INPUT = 'input.txt'
+
+patterns = File.read(INPUT).split("\n\n").map { _1.split("\n").map { |row| row.split('') } }
+
+def diff(r, x)
+  a, b = r[0...x].reverse.join(''), r[x..].join('')
+  m = [a.size, b.size].min
+
+  a[0...m].chars.zip(b[0...m].chars).count { _1 != _2 }
+end
+
+def col(p, d) = (1...(p.first.size)).to_a.find { |i| p.sum { diff(_1, i) } == d } || 0
+def row(p, d) = col(p.transpose, d)
+
+p patterns.sum { col(_1, 0) + row(_1, 0) * 100 }
+p patterns.sum { col(_1, 1) + row(_1, 1) * 100 }
