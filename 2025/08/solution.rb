5ba0c03ef37b73e92d5d2c1e00fe33f3127a1639
Alexander Lazarov <alexander.lazaroff@gmail.com>
2025-12-08 08:07:53 +0200

    Tidy up 2025/08

--- 2025/08/solution.rb
+++ 2025/08/solution.rb
@@ -3,36 +3,23 @@ input, rounds = 'input.txt', 1000
 junctions = File.readlines(input).map{ it.split(',').map(&:to_i) }
 circuits = junctions.map { [it] }
 
-distances = junctions.combination(2).map do |j1, j2|
-  [Math.sqrt(j1.zip(j2).map { (_1 - _2) ** 2 }.reduce(:+)), j1, j2]
-end.sort_by { it[0] }
+distances = 
+  junctions.
+    combination(2).
+    map { |j1, j2| [j1.zip(j2).map { (_1 - _2) ** 2 }.reduce(:+), j1, j2] }.
+    sort_by { it[0] }
 
-distances.take(rounds).map { [it[1], it[2]] }.each do |j1, j2|
+distances.each.with_index do |(_, j1, j2), i|
   c1 = circuits.find { it.include?(j1) }
   c2 = circuits.find { it.include?(j2) }
 
-  next if c1 == c2
-
-  circuits.delete(c1)
-  circuits.delete(c2)
-  circuits.push(c1 + c2)
-end
-
-p circuits.map { it.size }.sort.reverse.take(3).reduce(:*)
-
-distances.each do |d, j1, j2|
-  c1 = circuits.find { it.include?(j1) }
-  c2 = circuits.find { it.include?(j2) }
-
+  p circuits.map { it.size }.sort.reverse.take(3).reduce(:*) if i == rounds - 1
   next if c1 == c2
 
   circuits.delete(c1)
   circuits.delete(c2)
   circuits.push(c1 + c2)
 
-  if circuits.size == 1
-    p j1[0] * j2[0]
-    break
-  end
+  (p j1[0] * j2[0]) && break if circuits.size == 1
 end
 
