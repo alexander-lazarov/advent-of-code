d0183a8b3f955a5cabf1ce192a804e45d04fdb26
Alexander Lazarov <alexander.lazaroff@gmail.com>
2025-12-08 08:36:28 +0200

    Fix edge case for 2025/08
    The solution won't give a correct answer for part 1 in case the 1000th
    connection happens to merge two unmerged circuits that also happen to
    affect the top 3 biggest circuits.

--- 2025/08/solution.rb
+++ 2025/08/solution.rb
@@ -13,13 +13,13 @@ distances.each.with_index do |(_, j1, j2), i|
   c1 = circuits.find { it.include?(j1) }
   c2 = circuits.find { it.include?(j2) }
 
+  if c1 != c2
+    circuits.delete(c1)
+    circuits.delete(c2)
+    circuits.push(c1 + c2)
+  end
+
   p circuits.map { it.size }.sort.last(3).reduce(:*) if i == rounds - 1
-  next if c1 == c2
-
-  circuits.delete(c1)
-  circuits.delete(c2)
-  circuits.push(c1 + c2)
-
   (p j1[0] * j2[0]) && break if circuits.size == 1
 end
 
